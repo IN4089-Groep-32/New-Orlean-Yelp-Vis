e97c874fabe3fe34d2cce276ce5caedabdf97c3f
tyfann <1342344773@qq.com>
2023-01-06 10:56:53 +0100

    ins changed.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,12 @@
 ﻿<template>
     <div>
+        <div id="header-wrap" class="item-title" style="background: #540D6E;">
+            <div class="main-header_logo" id="logo" >
+                <a href="/">yelp</a>
+            </div>
+            <!-- <header class="grid-container">
+            </header> -->
+        </div>
         <NavBar class="nav" />
         <router-view class="map">router view</router-view>
         <!-- <router-view class="content">content view</router-view> -->
@@ -67,7 +74,31 @@ div {
 .nav {
     left: 0px;
     width: 100%;
-    height: 15%;
+    height: 10%;
+}
+
+.item-title {
+    padding-left: 5%;
+    padding-top: 1%;
+    width: 100%;
+    height: 10%;
+    position: relative;
+    
+}
+
+.main-header_logo {
+    width: 80px;
+    height: 40px;
+    background-repeat: no-repeat;
+    background-size: 80px 40px;
+    background-image: url(https://s3-media0.fl.yelpcdn.com/assets/public/logo_desktop_white.yji-2d2a7a4342fcfc9007c4020b1f76558a.svg);
+}
+
+.main-header_logo a {
+    display: block;
+    width: 100%;
+    height: 100%;
+    text-indent: -1000em;
 }
 
 </style>
